6189cfdaf50f979e6f4f69c648d7926df3e6b213
2601034r <2601034r@student.gla.ac.uk>
2021-07-27 15:26:31 +0530

    About Us Feature

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    "<a href='/rango/'>Index</a>"
+    return HttpResponse("Rango says hey there partner!")
+def about(request):
+    "<a href='/rango/about/'>About</a>"
+    return HttpResponse("Rango says here is the about page!")
